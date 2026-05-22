84eb56b395cbe7fdd8b65da65d94609e2d2aebe2
Castlabs iOS CI <ios-devs@castlabs.com>
2026-05-22 13:59:34 +0200

    Update Protobuf binary target to 3.13.0

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 struct PackageMetadata {
-    static let version: String = ""
-    static let checksum: String = ""
-    static let url: String = ""
+    static let version: String = "3.13.0"
+    static let checksum: String = "6e0c65565bcde7c57f9529a726709a4c91ff35efcb50ad68c51dec410ed33e05"
+    static let url: String = "https://cocoapods.players.castlabs.com/deps/protobuf/v3.13.0/protobuf.xcframework.zip"
 }
 
 let package = Package(
